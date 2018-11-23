8da9da800d63e6497e56355b7f66092eddf7339a
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-11-23 03:16:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,15 @@ def prime?(n)
   factor = 2 
   
   if n > 1 
-    return true 
+    while n > 1 
+      if n % factor == 0 
+        prime_numbers << factor
+        return true
+      
+      else
+        f += 1 
+      end
+    end
   else
     return false 
   end
